1080858148f28da622ebbc7c2e3b220b2c463cfd
Ruslan Shaydulin <rshaydu@g.clemson.edu>
2018-04-09 18:58:20 -0400

    performance improvements

--- packages/zoltan/src/phg/rs_amg_match.cpp
+++ packages/zoltan/src/phg/rs_amg_match.cpp
@@ -41,7 +41,7 @@ void RS_print_neighbors(HGraph* hg, int vtx, const std::unordered_set<ZOLTAN_GNO
 	std::cout << std::endl << "Total neighbors for "<< vtx << " : " << neigh_c.size() + neigh_f.size() << std::endl;
 }
 
-int RS_get_conditional_degree(HGraph* hg, int vtx, const std::unordered_set<ZOLTAN_GNO_TYPE>& coarse, const std::unordered_set<ZOLTAN_GNO_TYPE>& fine, bool in_coarse){
+int RS_get_conditional_degree(HGraph* hg, int vtx, const std::vector<bool>& is_coarse, bool in_coarse){
 	std::unordered_set<ZOLTAN_GNO_TYPE> neigh;
 	 /* for every hyperedge containing the vertex vtx */
 	for (ZOLTAN_GNO_TYPE i = hg->vindex[vtx]; i < hg->vindex[vtx+1]; i++) {
@@ -51,11 +51,11 @@ int RS_get_conditional_degree(HGraph* hg, int vtx, const std::unordered_set<ZOLT
 			ZOLTAN_GNO_TYPE v2 = hg->hvertex[j];				
 			if(in_coarse){
 				// vtx in coarse
-				if(vtx == v2 || coarse.find(v2) != coarse.end()){ continue; }
+				if(vtx == v2 || is_coarse[v2]){ continue; }
 				neigh.insert(v2);
 			}else{
 				// vtx in fine
-				if(vtx == v2 || fine.find(v2) != fine.end()){ continue; }
+				if(vtx == v2 || !is_coarse[v2]){ continue; }
 				neigh.insert(v2);
 			}
 		}	
@@ -73,7 +73,7 @@ unsigned int RS_get_max_vertex_weight(HGraph* hg){
 }
 
 // sees if there's enough neighbors from a given set
-bool RS_strongly_coupled_to_set(HGraph *hg, ZOLTAN_GNO_TYPE v1, RS_ALG_COORD_TYPE** vertex_alg_coords, float* new_hedge_weights, int R, const std::unordered_set<ZOLTAN_GNO_TYPE>& coarse){
+bool RS_strongly_coupled_to_set(HGraph *hg, ZOLTAN_GNO_TYPE v1, RS_ALG_COORD_TYPE** vertex_alg_coords, float* new_hedge_weights, int R, const std::vector<bool>& is_coarse){
 	ZOLTAN_GNO_TYPE v2, e;
 	RS_ALG_COORD_TYPE sum_to_c = 0.0; // sum of alg_dist weights to coarse vertices
 	RS_ALG_COORD_TYPE sum_all = 0.0; // sum of alg_dist weights to coarse vertices
@@ -87,7 +87,7 @@ bool RS_strongly_coupled_to_set(HGraph *hg, ZOLTAN_GNO_TYPE v1, RS_ALG_COORD_TYP
 			v2 = hg->hvertex[j];
 			if (v2 == v1) { continue; }
 			sum_all += new_hedge_weights[e];
-			if (coarse.find(v2) != coarse.end()){ 
+			if (is_coarse[v2]){ 
 				sum_to_c += new_hedge_weights[e];
 			}
 		}	
@@ -100,7 +100,7 @@ bool RS_strongly_coupled_to_set(HGraph *hg, ZOLTAN_GNO_TYPE v1, RS_ALG_COORD_TYP
 }
 
 // assumes vertex_alg_coords is an R by hg->nVtx sized 2d array
-int RS_compute_future_volumes(HGraph *hg, RS_ALG_COORD_TYPE** vertex_alg_coords, float* new_hedge_weights, int R, std::vector<RS_ALG_COORD_TYPE>& future_volumes, const std::unordered_set<ZOLTAN_GNO_TYPE>& coarse){
+int RS_compute_future_volumes(HGraph *hg, RS_ALG_COORD_TYPE** vertex_alg_coords, float* new_hedge_weights, int R, std::vector<RS_ALG_COORD_TYPE>& future_volumes, const std::vector<bool>& is_coarse){
 	char* yo = "RS_compute_future_volumes";
 #ifdef RS_INTEGER_FUTURE_VOLUMES 
 	std::cout << yo << "-> Using integer future volumes" << std::endl;
@@ -110,7 +110,7 @@ int RS_compute_future_volumes(HGraph *hg, RS_ALG_COORD_TYPE** vertex_alg_coords,
 	/* Now each vertex just gives itself to the most connected neighbor */
 
 	for (v1 = 0; v1 < hg->nVtx; v1++){
-		if (coarse.find(v1) != coarse.end()){
+		if (is_coarse[v1]){
 				continue;
 		}
 		int best = -1;
@@ -122,7 +122,7 @@ int RS_compute_future_volumes(HGraph *hg, RS_ALG_COORD_TYPE** vertex_alg_coords,
 			/* for every other vertex in the hyperedge */
 			for (ZOLTAN_GNO_TYPE j = hg->hindex[e]; j < hg->hindex[e+1]; j++) {
 				ZOLTAN_GNO_TYPE v2 = hg->hvertex[j];
-				if (v2 == v1 || coarse.find(v2) != coarse.end()) { continue; }
+				if (v2 == v1 || is_coarse[v2]) { continue; }
 				ips[v2] += (hg->ewgt ? hg->ewgt[e] : 1.0);
 			}	
 		}
@@ -152,7 +152,7 @@ int RS_compute_future_volumes(HGraph *hg, RS_ALG_COORD_TYPE** vertex_alg_coords,
 	std::vector<RS_ALG_COORD_TYPE> precomp_sums;
 	precomp_sums.reserve(hg->nVtx);
 	for (v1 = 0; v1 < hg->nVtx; v1++){
-		if (coarse.find(v1) != coarse.end()){
+		if (is_coarse[v1]){
 			precomp_sums.push_back(-1.0);
 			continue;
 		}
@@ -163,40 +163,36 @@ int RS_compute_future_volumes(HGraph *hg, RS_ALG_COORD_TYPE** vertex_alg_coords,
 			/* for every other vertex in the hyperedge */
 			for (j = hg->hindex[e]; j < hg->hindex[e+1]; j++) {
 				v2 = hg->hvertex[j];
-				if (v2 == v1 || coarse.find(v2) != coarse.end()) { continue; }
+				if (v2 == v1 || is_coarse[v2]) { continue; }
 				d_sum += (hg->ewgt ? hg->ewgt[e] : 1.0);
 			}	
 		}
 		precomp_sums.push_back(d_sum);
 	}
-
-	for (v1 = 0; v1 < hg->nVtx; v1++){
-		if (coarse.find(v1) != coarse.end()){
-			future_volumes.push_back(-1.0);
-			continue;
-		}
-		RS_ALG_COORD_TYPE fv = (RS_ALG_COORD_TYPE)(hg->vwgt ? hg->vwgt[v1] : 1.0);
-		/* for every hyperedge containing the vertex v */
-		for (i = hg->vindex[v1]; i < hg->vindex[v1+1]; i++) {
-			e = hg->vedge[i];
-			/* for every other vertex in the hyperedge */
-			for (j = hg->hindex[e]; j < hg->hindex[e+1]; j++) {
-				v2 = hg->hvertex[j];
-				if (v2 == v1) { continue; }
-				/* TODO put more code here */
-				fv += (hg->vwgt ? hg->vwgt[v2] : 1.0)*((hg->ewgt ? hg->ewgt[e] : 1.0) / precomp_sums[v2]); // if made [1] weighted, add weights here too
-			}
-		}
-		future_volumes.push_back(fv);
-	}
 	
+	for (e = 0; e < hg->nEdge; e++){
+		float curr_edgew = (hg->ewgt ? hg->ewgt[e] : 1.0);
+		/* for every vertex in the hyperedge */
+		for (j = hg->hindex[e]; j < hg->hindex[e+1]; j++) {
+			v1 = hg->hvertex[j];
+			if (is_coarse[v1]){ continue; }
+			/* for every other vertex in the hyperedge */
+			for (i = hg->hindex[e]; i < hg->hindex[e+1]; i++) {
+				v2 = hg->hvertex[i];
+				if (v2 == v1) { continue; }
+				future_volumes[v2] += (hg->vwgt ? hg->vwgt[v1] : 1.0) * (curr_edgew / precomp_sums[v1]);
+			}	
+		}	
+	}
+
 	return ZOLTAN_OK;	
 #endif
 }
 
-int RS_compute_preferences(ZZ *zz, HGraph *hg, RS_ALG_COORD_TYPE** vertex_alg_coords, float* new_hedge_weights, int R, std::unordered_map<ZOLTAN_GNO_TYPE, std::vector<ZOLTAN_GNO_TYPE>> &pref_dict_f, std::unordered_map<ZOLTAN_GNO_TYPE, std::vector<ZOLTAN_GNO_TYPE>> &pref_dict_c, const std::unordered_set<ZOLTAN_GNO_TYPE> &coarse, std::unordered_set<ZOLTAN_GNO_TYPE> &fine){
+int RS_compute_preferences(ZZ *zz, HGraph *hg, RS_ALG_COORD_TYPE** vertex_alg_coords, float* new_hedge_weights, int R, std::unordered_map<ZOLTAN_GNO_TYPE, std::vector<ZOLTAN_GNO_TYPE>> &pref_dict_f, std::unordered_map<ZOLTAN_GNO_TYPE, std::vector<ZOLTAN_GNO_TYPE>> &pref_dict_c, const std::vector<bool>& is_coarse){
 	ZOLTAN_GNO_TYPE i,j,v2,e;
-	for (auto &v1 : coarse){
+	for (ZOLTAN_GNO_TYPE v1 = 0; v1 < hg->nVtx; v1++){
+		if (!is_coarse[v1]){ continue; } 
 		std::unordered_map<ZOLTAN_GNO_TYPE, RS_ALG_COORD_TYPE> rm;
 		std::multimap<RS_ALG_COORD_TYPE, ZOLTAN_GNO_TYPE> m;
 		/* for every hyperedge containing the vertex v1 */
@@ -205,7 +201,7 @@ int RS_compute_preferences(ZZ *zz, HGraph *hg, RS_ALG_COORD_TYPE** vertex_alg_co
 			/* for every other vertex in the hyperedge */
 			for (j = hg->hindex[e]; j < hg->hindex[e+1]; j++) {
 				v2 = hg->hvertex[j];
-				if (v2 == v1 || coarse.find(v2) != coarse.end()) { continue; }
+				if (v2 == v1 || is_coarse[v2]) { continue; }
 				rm[v2] -= new_hedge_weights[e];
 			}	
 		}
@@ -220,7 +216,8 @@ int RS_compute_preferences(ZZ *zz, HGraph *hg, RS_ALG_COORD_TYPE** vertex_alg_co
 		pref_dict_c[v1] = v;
 	}
 	
-	for (auto &v1 : fine){
+	for (ZOLTAN_GNO_TYPE v1 = 0; v1 < hg->nVtx; v1++){
+		if (is_coarse[v1]){ continue; }
 		std::unordered_map<ZOLTAN_GNO_TYPE, RS_ALG_COORD_TYPE> rm;
 		std::multimap<RS_ALG_COORD_TYPE, ZOLTAN_GNO_TYPE> m;
 		/* for every hyperedge containing the vertex v1 */
@@ -229,7 +226,7 @@ int RS_compute_preferences(ZZ *zz, HGraph *hg, RS_ALG_COORD_TYPE** vertex_alg_co
 			/* for every other vertex in the hyperedge */
 			for (j = hg->hindex[e]; j < hg->hindex[e+1]; j++) {
 				v2 = hg->hvertex[j];
-				if (v2 == v1 || fine.find(v2) != fine.end()) { continue; }
+				if (v2 == v1 || !is_coarse[v2]) { continue; }
 				rm[v2] -= new_hedge_weights[e];
 			}	
 		}
@@ -248,6 +245,8 @@ int RS_compute_preferences(ZZ *zz, HGraph *hg, RS_ALG_COORD_TYPE** vertex_alg_co
 
 int RS_amg_matching(ZZ *zz, HGraph *hg, PHGPartParams *hgp, ZOLTAN_GNO_TYPE *match, RS_ALG_COORD_TYPE** vertex_alg_coords, float* new_hedge_weights, int R){
 	int err;
+	int num_coarse = -1;
+	int num_fine = -1;
 	char* yo = "RS_amg_matching";
 
 	if (rs_total_vwgt == -1.0){
@@ -267,15 +266,10 @@ int RS_amg_matching(ZZ *zz, HGraph *hg, PHGPartParams *hgp, ZOLTAN_GNO_TYPE *mat
 
 	/* ----------- stage one: compute seeds ------------- */
 
-	std::unordered_set<ZOLTAN_GNO_TYPE> coarse, fine;
+	std::vector<bool> is_coarse(hg->nVtx);
 
-#ifdef RS_INTEGER_FUTURE_VOLUMES 
 	std::vector<RS_ALG_COORD_TYPE> future_volumes(hg->nVtx, 0.0);
-#else
-	std::vector<RS_ALG_COORD_TYPE> future_volumes;
-	future_volumes.reserve(hg->nVtx);
-#endif	
-	err = RS_compute_future_volumes(hg, vertex_alg_coords, new_hedge_weights, R, future_volumes, coarse);
+	err = RS_compute_future_volumes(hg, vertex_alg_coords, new_hedge_weights, R, future_volumes, is_coarse);
 	
 	if (err != ZOLTAN_OK) { return ZOLTAN_FATAL; }
 
@@ -295,38 +289,37 @@ int RS_amg_matching(ZZ *zz, HGraph *hg, PHGPartParams *hgp, ZOLTAN_GNO_TYPE *mat
 
 	for (int v = 0; v < future_volumes.size(); v++){
 			if (future_volumes[v] > fv_threshold){
-				coarse.insert(v);
+				is_coarse[v] = true;
 			}else{
-				fine.insert(v);
+				is_coarse[v] = false;
 			}
 	}
-	//std::cout << "After two standard deviations over mean: " << future_volumes.size() << " coarse: " << coarse.size()  << " fine: " << fine.size() << std::endl;	
+//	num_coarse = std::accumulate(is_coarse.begin(), is_coarse.end(), 0);	
+//	num_fine = hg->nVtx - num_coarse;
+//	std::cout << "After two standard deviations over mean: " << future_volumes.size() << " coarse: " << num_coarse  << " fine: " << num_fine << std::endl;	
 
 	/* Recompute future volumes */
 
-#ifdef RS_INTEGER_FUTURE_VOLUMES 
 	std::fill(future_volumes.begin(), future_volumes.end(), 0.0);
-#else
-	future_volumes.clear();
-#endif
 	
-	err = RS_compute_future_volumes(hg, vertex_alg_coords, new_hedge_weights, R, future_volumes, coarse);
+	err = RS_compute_future_volumes(hg, vertex_alg_coords, new_hedge_weights, R, future_volumes, is_coarse);
 	
 	if (err != ZOLTAN_OK) { return ZOLTAN_FATAL; }
 
 	/* Now, traverse fine vertices and add them to coarse if needed */
 
 	for (auto v : sort_indexes(future_volumes)){ // order can be different
-		if(coarse.find(v) != coarse.end()) { continue; }
-		if(!RS_strongly_coupled_to_set(hg, v, vertex_alg_coords, new_hedge_weights, R, coarse)){
-			coarse.insert(v);
-			fine.erase(fine.find(v));	
+		if(is_coarse[v]) { continue; }
+		if(!RS_strongly_coupled_to_set(hg, v, vertex_alg_coords, new_hedge_weights, R, is_coarse)){
+			is_coarse[v] = true;
 		}	
 	}
 
 	/* ------------- stage two ---------------------- */
 
-	//std::cout << "After strongly connected: " << future_volumes.size() << " coarse: " << coarse.size()  << " fine: " << fine.size() << std::endl;	
+//	num_coarse = std::accumulate(is_coarse.begin(), is_coarse.end(), 0);	
+//	num_fine = hg->nVtx - num_coarse;
+//	std::cout << "After strongly connected: " << future_volumes.size() << " coarse: " << num_coarse  << " fine: " << num_fine << std::endl;	
 
 #ifdef RS_STABLE_MATCHING
 	/* first, compute preferences and capacities */
@@ -334,15 +327,18 @@ int RS_amg_matching(ZZ *zz, HGraph *hg, PHGPartParams *hgp, ZOLTAN_GNO_TYPE *mat
 
 	std::unordered_map<ZOLTAN_GNO_TYPE, std::vector<ZOLTAN_GNO_TYPE>> pref_dict_f, pref_dict_c;
 	
-	RS_compute_preferences(zz, hg, vertex_alg_coords, new_hedge_weights, R, pref_dict_f, pref_dict_c, coarse, fine);
+	RS_compute_preferences(zz, hg, vertex_alg_coords, new_hedge_weights, R, pref_dict_f, pref_dict_c, is_coarse);
 	
 	std::unordered_map<ZOLTAN_GNO_TYPE, unsigned int> capacities_c; 
-	
-	unsigned int capacity = RS_get_max_vertex_weight(hg)*((fine.size() / coarse.size()) + 2) + 10;
+
+	int num_coarse = std::accumulate(is_coarse.begin(), is_coarse.end(), 0);	
+	int num_fine = hg->nVtx - num_coarse;
+	unsigned int capacity = RS_get_max_vertex_weight(hg)*(( num_fine / num_coarse) + 2) + 10;
 	
 	//std::cout << "Assigned capacity " << capacity << " with max vertex_weight " << RS_get_max_vertex_weight(hg) << std::endl;
 	
-	for (auto &i : coarse){
+	for (ZOLTAN_GNO_TYPE i = 0; i < hg->nVtx; i++){
+		if (!is_coarse[i]) { continue; }
 		capacities_c[i] = capacity;
 	}
 
@@ -371,7 +367,8 @@ int RS_amg_matching(ZZ *zz, HGraph *hg, PHGPartParams *hgp, ZOLTAN_GNO_TYPE *mat
 		}
 	}
 
-	for(auto &v: coarse){
+	for(ZOLTAN_GNO_TYPE v = 0; v < hg->nVtx; v++){
+		if (!is_coarse[v]){ continue; }
         if (assignment.find(v) == assignment.end()){
             match[v] = v;
         }
@@ -387,13 +384,17 @@ int RS_amg_matching(ZZ *zz, HGraph *hg, PHGPartParams *hgp, ZOLTAN_GNO_TYPE *mat
 		match[i] = i;
 	}
 
-	std::unordered_map<ZOLTAN_GNO_TYPE, float> clwgts; // cluster weights
+	std::vector<float> clwgts(hg->nVtx); // cluster weights
+	std::vector<float> ips(hg->nVtx); // inner products
+	std::vector<float> adj(hg->nVtx);
+	ZOLTAN_GNO_TYPE n; // number of neighbors
 
-	for (auto &v1 : fine){
-
+	for (ZOLTAN_GNO_TYPE v1 = 0; v1 < hg->nVtx; v1++){
+		if (is_coarse[v1]) { continue; }
 		int best = -1;
 		float best_w = 0;
-		std::unordered_map<ZOLTAN_GNO_TYPE, float> ips;
+		n = 0;
+
 		/* for every hyperedge containing the vertex v1 */
 		for (ZOLTAN_GNO_TYPE i = hg->vindex[v1]; i < hg->vindex[v1+1]; i++) {
 			ZOLTAN_GNO_TYPE e = hg->vedge[i];
@@ -401,24 +402,28 @@ int RS_amg_matching(ZZ *zz, HGraph *hg, PHGPartParams *hgp, ZOLTAN_GNO_TYPE *mat
 			for (ZOLTAN_GNO_TYPE j = hg->hindex[e]; j < hg->hindex[e+1]; j++) {
 				ZOLTAN_GNO_TYPE v2 = hg->hvertex[j];
 				if (v2 == v1) { continue; }
-				if (fine.find(v2) != fine.end()){
+				if (!is_coarse[v2]){
 				//	if (match[v2] == v2) { continue; }
 				//	ips[match[v2]] += new_hedge_weights[e];
 					continue;
 				}
+				if (ips[v2] == 0.0)
+						adj[n++] = v2;
 				ips[v2] += new_hedge_weights[e];
 			}	
 		}
-		for (auto &i : ips){
-			float clwgt = hg->vwgt[v1] + hg->vwgt[i.first]; // prospective cluster weight
-			if (clwgts.find(i.first) != clwgts.end()){
-					clwgt += clwgts[i.first];
+
+		for (int i = 0; i < n; i++){
+			ZOLTAN_GNO_TYPE v2 = adj[i];
+			float clwgt = hg->vwgt[v1] + hg->vwgt[v2]; // prospective cluster weight
+			if (clwgts[v2] != 0){
+					clwgt += clwgts[v2];
 			}
-			//i.second /= clwgt;
-			if (i.second > best_w && (clwgt < rs_total_vwgt / zz->LB.Num_Global_Parts)){
-				best = i.first;
-				best_w = i.second;
+			if (ips[v2] > best_w && (clwgt < rs_total_vwgt / zz->LB.Num_Global_Parts)){
+				best = v2;
+				best_w = ips[v2];
 			}
+			ips[v2] = 0.0;
 		}
 		if (best == -1 || best_w == 0){
 			std::cout << "Couldn't find a match for " << v1 << " with weight " << hg->vwgt[v1] << " out of " << ips.size() << " candidates" << std::endl;
